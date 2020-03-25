29380b9a50b86e056bc12c71441cfe77f2f0a0a7
espinolaramd <espinolaramd@hartwick.edu>
2020-03-25 02:34:47 -0700

    update

--- drive.py
+++ drive.py
@@ -6,12 +6,17 @@ class Queue:
         self.myqueue = LinkedListTail()
 
     def push(self, data):
+        self.myqueue.push_head(data)
+
+    def push_end(self,data):
         self.myqueue.push_end(data)
 
     def pop(self):
-        self.myqueue.remove_head()
+        self.myqueue.pop_head()
+
 
 myqueve = Queue()
 myqueve.push(5)
 myqueve.push(10)
+myqueve.push_end(10)
 print(myqueve.pop())
--- list.py
+++ list.py
@@ -2,16 +2,19 @@
 #3.5.2020
 # queve class
 
-class Node:
-    def __init__(self,data):
+class ListNode:
+    def __init__(self, data=None):
         self.data = data
         self.next = None
+
+    def __repr__(self):
+        return repr(self.data)
+
 
 class LinkedListTail:
     def __init__(self):
         self.head = None
-        self.tail = Node(None)
-
+        self.tail = None
 
     def __repr__(self):
         nodes = []
@@ -22,7 +25,7 @@ class LinkedListTail:
         return str(nodes)
 
     def push_head(self, data):
-        new_node = Node(data=data)
+        new_node = ListNode(data=data)
         if self.head is not None:
             new_node.next = self.head
             self.head = new_node
@@ -32,7 +35,7 @@ class LinkedListTail:
             self.tail = self.head
 
     def push_end(self, data):
-        new_node = Node(data)
+        new_node = ListNode(data=data)
         if self.head is not None:
             self.tail.next = new_node
             self.tail = new_node
@@ -40,9 +43,17 @@ class LinkedListTail:
             self.head = new_node
             self.tail = self.head
 
-    def remove_head(self):
+    def pop_head(self):
         new_node = self.head
-        self.head = self.head.next
+        self.head = new_node.next
         return new_node.data
 
+    def pop_end(self):
+        current_node = self.head
+        previous_node = self.head
+        while current_node.next:
+            previous_node = current_node
+            current_node = current_node.next
+        previous_node.next = None
+        return current_node.data
 
